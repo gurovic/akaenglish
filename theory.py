c60a8d0f0540fc34b0b13ea367773a67f375fb17
Vassiliev-Valeriy <Valera.Vassiliev@gmail.com>
2015-04-29 11:22:19 +0400

    Added opening tasks after theory

--- theory.py
+++ theory.py
@@ -9,10 +9,35 @@ theory = th.read()
 theory_parts = theory.count('----------') + 1
 theory_list = list(map(str, theory.split('----------'))) 
 
+def end_task(exit_window):
+    exit_window.destroy()
+    
+def open_tasks(exit_window):
+    exit_window.destroy()
+    root.destroy()
+    import correcting_programm
+    execfile('correcting_programm.py')
+
 def excercise_room_plus():
     global exercise_number
     if exercise_number < theory_parts:
         exercise_number += 1
+    
+    else:
+        exit_window = Tk()
+        exit_window.title('Выход')
+        exit_window.geometry('250x100+575+210')
+        exit_window.resizable(False, False)
+        quit_question = Label(exit_window, text = "Вы хотите выполнить упражнения", font = 'tahoma 11')
+        quit_question_line = Label(exit_window, text = "по изученной теории?", font = 'tahoma 11')
+        quit_yes = Button(exit_window, text = 'Да', width = 5, height = 1, font = 'tahoma 14', command = lambda: open_tasks(exit_window), fg = 'green')
+        quit_no = Button(exit_window, text = 'Нет', width = 5, height = 1, font = 'tahoma 14', command = lambda: end_task(exit_window), fg = 'red')
+        quit_question.place(x = 1, y = 5)
+        quit_question_line.place(x = 43, y = 25)
+        quit_yes.place(x = 55, y = 50)
+        quit_no.place(x = 130, y = 50)
+        exit_window.mainloop()
+        
     room["text"] = exercise_number
     theory_text.delete('1.0', END)
     theory_text.insert(1.0, theory_list[exercise_number - 1])
